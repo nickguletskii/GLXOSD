66f5b9e51c01ad80587091d0a504dcb704200bb6
Nick Guletskii <nickguletskii200@gmail.com>
2013-09-12 20:16:58 +0300

    Installation strategy changes. Preparation for Debian packaging.
    Now installs into the install prefix specified by cmake. SO versions added.

--- write_preload.sh
+++ write_preload.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-OUT_FILE="glxosd"
+OUT_FILE=$1/glxosd
 echo "#! /bin/sh" > ${OUT_FILE}
-echo "INJ_PATH="$PWD >> ${OUT_FILE}
-echo "LD_LIBRARY_PATH=\$INJ_PATH/bin:\$LD_LIBRARY_PATH LD_PRELOAD=libelfhacks.so:libglinject.so:libglxosd.so:libftgl.so:\$LD_PRELOAD \$@" >> ${OUT_FILE}
+echo "INJ_PATH="$2 >> ${OUT_FILE}
+echo "LD_LIBRARY_PATH=\$INJ_PATH/lib/glxosd:\$LD_LIBRARY_PATH LD_PRELOAD=libglxosd-elfhacks.so:libglxosd-glinject.so:libglxosd.so:libglxosd-ftgl.so:\$LD_PRELOAD \$@" >> ${OUT_FILE}
 chmod +x ${OUT_FILE}
