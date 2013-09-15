83c8caea3f8f295d2311327337162e56aeb9edcb
Nick Guletskii <nickguletskii200@gmail.com>
2013-09-15 18:33:24 +0300

    Installation strategy change
    CMake now installs ${CMAKE_INSTALL_PREFIX}/bin/glxosd[-platformsuffix]

--- write_preload.sh
+++ write_preload.sh
@@ -1,6 +1,17 @@
 #! /bin/sh
-OUT_FILE=$1/glxosd
+TARGET_DIR=$1
+PREFIX=$2
+ARCH_SUFFIX=$3
+PLATFORM_SUFFIX=$4
+OUT_FILE=$TARGET_DIR/glxosd$PLATFORM_SUFFIX
 echo "#! /bin/sh" > ${OUT_FILE}
-echo "INJ_PATH="$2 >> ${OUT_FILE}
-echo "LD_LIBRARY_PATH=\$INJ_PATH/lib/glxosd:\$LD_LIBRARY_PATH LD_PRELOAD=libglxosd-elfhacks.so:libglxosd-glinject.so:libglxosd.so:libglxosd-ftgl.so:\$LD_PRELOAD \$@" >> ${OUT_FILE}
+echo "LD_LIBRARY_PATH="$PREFIX"/lib/"$ARCH_SUFFIX"/glxosd:\$LD_LIBRARY_PATH LD_PRELOAD=libglxosd-elfhacks.so:libglxosd-glinject.so:libglxosd.so:libglxosd-ftgl.so:\$LD_PRELOAD \$@" >> ${OUT_FILE}
+chmod +x ${OUT_FILE}
+OUT_FILE=$TARGET_DIR/glxosd
+echo "#! /bin/sh" > ${OUT_FILE}
+if [ $PLATFORM_SUFFIX = "-amd64" ]; then
+    echo "LD_LIBRARY_PATH="$PREFIX"/lib/x86_64-linux-gnu/glxosd:"$PREFIX"/lib/i386-linux-gnu/glxosd:\$LD_LIBRARY_PATH LD_PRELOAD=libglxosd-elfhacks.so:libglxosd-glinject.so:libglxosd.so:libglxosd-ftgl.so:\$LD_PRELOAD \$@" >> ${OUT_FILE}
+elif [ $PLATFORM_SUFFIX = "-i386" ]; then
+    echo "LD_LIBRARY_PATH="$PREFIX"/lib/i386-linux-gnu/glxosd:\$LD_LIBRARY_PATH LD_PRELOAD=libglxosd-elfhacks.so:libglxosd-glinject.so:libglxosd.so:libglxosd-ftgl.so:\$LD_PRELOAD \$@" >> ${OUT_FILE}
+fi
 chmod +x ${OUT_FILE}
