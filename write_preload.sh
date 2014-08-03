b8c19316e07961a39c56934cbadc7b7fa0ac3ea7
Nick Guletskii <nickguletskii200@gmail.com>
2014-08-03 18:10:03 +0300

    Add the ability to change the install path for different distros

--- write_preload.sh
+++ write_preload.sh
@@ -2,7 +2,6 @@
 GLXOSD_SRC_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TEMPLATE=$(cat ${GLXOSD_SRC_ROOT}/launcher_template.sh)
 TARGET_FILE="./glxosd"
-PREFIX="/usr"
 while : ; do
 	case "$1" in 
 		--target-file)
@@ -10,8 +9,16 @@ while : ; do
 			TARGET_FILE="$2"
 			shift 2 ;;
 		--installation-prefix)
-		[ -n "${PREFIX}" ]
-			PREFIX="$2"
+		[ -n "${INSTALL_PREFIX}" ]
+			INSTALL_PREFIX="$2"
+			shift 2 ;;
+		--installation-suffix-32)
+		[ -n "${INSTALL_SUFFIX_32}" ]
+			INSTALL_SUFFIX_32="$2"
+			shift 2 ;;
+		--installation-suffix-64)
+		[ -n "${INSTALL_SUFFIX_64}" ]
+			INSTALL_SUFFIX_64="$2"
 			shift 2 ;;
 		*)
 			break ;;
@@ -20,8 +27,8 @@ done
 function escape {
 	echo $(echo $1 | sed -e 's/[\/&]/\\&/g')
 }
-OUTPUT=$TEMPLATE;
-OUTPUT=$(echo "$OUTPUT" | sed "s/#{{GLXOSD_LIBRARY_PATH_I386}}/"$(escape $PREFIX)"\/lib\/i386-linux-gnu\/glxosd/g");
-OUTPUT=$(echo "$OUTPUT" | sed "s/#{{GLXOSD_LIBRARY_PATH_AMD64}}/"$(escape $PREFIX)"\/lib\/x86_64-linux-gnu\/glxosd/g");
-echo "$OUTPUT" > $TARGET_FILE;
-chmod +x $TARGET_FILE
+OUTPUT="${TEMPLATE}";
+OUTPUT=$(echo "${OUTPUT}" | sed "s/#{{GLXOSD_LIBRARY_PATH_I386}}/"$(escape ${INSTALL_PREFIX-"/usr/"})"\/"$(escape ${INSTALL_SUFFIX_32-"/lib/i386-linux-gnu/"})"\/gloxsd\//g");
+OUTPUT=$(echo "${OUTPUT}" | sed "s/#{{GLXOSD_LIBRARY_PATH_AMD64}}/"$(escape ${INSTALL_PREFIX-"/usr/"})"\/"$(escape ${INSTALL_SUFFIX_64-"/lib/x86_64-linux-gnu/"})"\/gloxsd\//g");
+echo "${OUTPUT}" > "${TARGET_FILE}"
+chmod +x "${TARGET_FILE}"
