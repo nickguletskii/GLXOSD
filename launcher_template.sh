de957bd146369e73dd29e6906cf828ee6f5b9e32
Nick Guletskii <nick@nickguletskii.com>
2016-03-09 20:34:38 +0300

    Update installation procedure to be compatible with version 3

--- launcher_template.sh
+++ launcher_template.sh
@@ -48,21 +48,9 @@ fi
 GLXOSD_LIBRARY_PATH_I386="{{GLXOSD_LIBRARY_PATH_I386}}"
 GLXOSD_LIBRARY_PATH_AMD64="{{GLXOSD_LIBRARY_PATH_AMD64}}"
 
-if [ -d ${GLXOSD_LIBRARY_PATH_I386} ]; then
-	GLXOSD_I386_PLUGIN_LIST=$(ls ${GLXOSD_LIBRARY_PATH_I386}/libglxosd-plugins-*.so | xargs -n 1 basename)
-	GLXOSD_I386_PLUGIN_LIST=$(echo ${GLXOSD_I386_PLUGIN_LIST} | sed 's/ /:/g')
-fi
-
-if [ -d ${GLXOSD_LIBRARY_PATH_AMD64} ]; then
-	GLXOSD_AMD64_PLUGIN_LIST=$(ls ${GLXOSD_LIBRARY_PATH_AMD64}/libglxosd-plugins-*.so | xargs -n 1 basename)
-	GLXOSD_AMD64_PLUGIN_LIST=$(echo ${GLXOSD_AMD64_PLUGIN_LIST} | sed 's/ /:/g')
-fi
-
 export LD_LIBRARY_PATH="${STEAM_OVERLAY_LIB_PATHS}:${GLXOSD_LIBRARY_PATH_I386}:${GLXOSD_LIBRARY_PATH_AMD64}:${LD_LIBRARY_PATH}"
-export LD_PRELOAD="${GLXOSD_PRELOAD}:${STEAM_OVERLAY_LIBS}:libglxosd-elfhacks.so:libglxosd-glinject.so:libglxosd.so:${LD_PRELOAD}"
-GLXOSD_PLUGINS="${GLXOSD_I386_PLUGIN_LIST}:${GLXOSD_AMD64_PLUGIN_LIST}"
-GLXOSD_PLUGINS=$(echo ${GLXOSD_PLUGINS} | tr ':' '\n' | sort | uniq | tr '\n' ':')
-export GLXOSD_PLUGINS
+export LD_PRELOAD="${GLXOSD_PRELOAD}:${STEAM_OVERLAY_LIBS}:libglxosd-elfhacks.so:libglxosd-glinject.so:libglxosd-freetype-gl.so:libluajit-5.1.so:${LD_PRELOAD}"
+export GLINJECT_BOOTSTRAP_SCRIPT="{{GLXOSD_SHARED_PATH}}/glxosd/init.lua"
 
 export GLINJECT_CONSTRUCTORS="constructGLXOSD"
 export GLINJECT_DESTRUCTORS="destructGLXOSD"
