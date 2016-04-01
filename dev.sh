cbe0119c2a073df2a24addedde1edb082e2f8fe7
Nick Guletskii <nick@nickguletskii.com>
2016-04-01 19:04:38 +0300

    Update packaging for version 3.0.3

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-VERSION=3.0.1
+VERSION=3.0.3
 echo -e "[\e[32mrebuild.sh\e[00m] Installing required packages..."
 sudo apt-get install git debhelper pbuilder dh-make debootstrap devscripts
 if [ ! -d glxosd-$VERSION-orig ]
