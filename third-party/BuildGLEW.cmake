1908879c17b135211993044ef9a0c56f9f0f45ff
Nick Guletskii <nick@nickguletskii.com>
2016-05-01 23:41:05 +0300

    Fix GLEW build

--- third-party/BuildGLEW.cmake
+++ third-party/BuildGLEW.cmake
@@ -1,23 +1,14 @@
 include(CMakeParseArguments)
 include(ExternalProject)
 
-set(INSTALL_COMMAND
-	make
-	CFLAGS=-fPIC
-	LIBDIR=${CMAKE_CURRENT_BINARY_DIR}/glew-1.13.0/lib
-	GLEW_DEST=${CMAKE_CURRENT_BINARY_DIR}/glew-1.13.0
-	install)
-
 ExternalProject_Add(project_glew
 	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/glew-1.13.0
-	URL ${PROJECT_SOURCE_DIR}/third-party/glew-1.13.0.tgz
-	CONFIGURE_COMMAND ""
-	BUILD_IN_SOURCE 1
-	BUILD_COMMAND ""
-	INSTALL_COMMAND "${INSTALL_COMMAND}")
+	SOURCE_DIR ${PROJECT_SOURCE_DIR}/third-party/glew-1.13.0/build/cmake/
+	CMAKE_ARGS -D BUILD_UTILS=OFF -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/glew-1.13.0 -DCMAKE_C_FLAGS=-fPIC
+)
 
 ExternalProject_Get_Property(project_glew install_dir)
-include_directories(${install_dir}/include/)
+include_directories(${install_dir}/include)
 add_library(glew STATIC IMPORTED)
 set_property(TARGET glew PROPERTY IMPORTED_LOCATION ${install_dir}/lib/libGLEW.a)
 add_dependencies(glew project_glew)
