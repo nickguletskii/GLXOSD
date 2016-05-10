46d92d46e587d2198d111244c007717228c39e29
Nick Guletskii <nick@nickguletskii.com>
2016-05-10 18:00:52 +0300

    Remove Google analytics

--- js/common.js
+++ js/common.js
@@ -4,35 +4,3 @@ require(['jquery', 'foundation'], function ($, foundation) {
 	$(document)
 		.foundation();
 });
-define(function (require) {
-
-	var module,
-		gaName = "ga"; // Global name of analytics object. Defaults to `ga`.
-
-	// Setup temporary Google Analytics objects.
-	window.GoogleAnalyticsObject = gaName;
-	window[gaName] = function () {
-		(window[gaName].q = window[gaName].q || [])
-			.push(arguments);
-	};
-	window[gaName].l = 1 * new Date();
-
-	// Immediately add a pageview event to the queue.
-	window[gaName]("create", "UA-46764899-1", "auto");
-	window[gaName]("send", "pageview");
-
-	// Create a function that wraps `window[gaName]`.
-	// This allows dependant modules to use `window[gaName]` without knowingly
-	// programming against a global object.
-	module = function () {
-		window[gaName].apply(this, arguments);
-	};
-
-	// Asynchronously load Google Analytics, letting it take over our `window[gaName]`
-	// object after it loads. This allows us to add events to `window[gaName]` even
-	// before the library has fully loaded.
-	require(["//www.google-analytics.com/analytics.js"]);
-
-	return module;
-
-});
